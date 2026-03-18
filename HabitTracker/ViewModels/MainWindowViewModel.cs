7ac9c72c1dcde35cf50c6dba9c07146d23db5c5f
jbile <jbile@JAKUB>
2026-03-18 13:54:09 +0100

    Usuniecie avalszponi

--- HabitTracker/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HabitTracker.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- HabitTracker/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace HabitTracker.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
