a459e94349fbac2bec9ec22b7b251b8c4c3d2660
MaciejBanys1 <linnar341@gmail.com>
2026-05-07 09:54:16 +0200

    Dodanie oprawy graficznej dla ustawien

---
+++ HabitTracker/Views/ChangePasswordWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace HabitTracker.Views;
+
+public partial class ChangePasswordWindow : Window
+{
+    public ChangePasswordWindow()
+    {
+        InitializeComponent();
+    }
+}
